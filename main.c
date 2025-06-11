24adcbabac0c390197f21db4627c6c5d178237c5
Ethan Hackney <ethanhackney@pop-os.lan>
2025-06-11 11:30:38 -0700

    added some tests

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
+#define PLD_HASH_MAP_INIT_CAP 1
 #include "include/pld_hash_map.h"
 #include <stdio.h>
+#include <assert.h>
 
 static inline hash_map_size_t
 inthash(int i)
@@ -22,26 +24,34 @@ PLD_HASH_MAP_DEFINE(int, int, int2intmap, inthash, intcmp)
 int
 main(void)
 {
+        int log2[] = {
+                -1, /* nil */
+                 0, /* 1 */
+                 1, /* 2 */
+                 2, /* 4 */
+                 3, /* 8 */
+                 4, /* 16 */
+                -1, /* nil */
+        };
         struct int2intmap *i2imap = NULL;
         int *vp = NULL;
+        int i = 0;
 
         i2imap = int2intmap_new(0);
         if (i2imap == NULL)
                 puts("here");
 
-        if (int2intmap_set(&i2imap, 16, 4) < 0)
-                puts("here");
+        for (i = 1; log2[i] >= 0; i++)
+                assert(int2intmap_set(&i2imap, i, log2[i]) == 0);
 
-        vp = int2intmap_get(i2imap, 16);
-        if (vp != NULL)
-                printf("%d\n", *vp);
+        for (i = 1; log2[i] >= 0; i++) {
+                vp = int2intmap_get(i2imap, i);
+                assert(vp != NULL);
+                assert(*vp == log2[i]);
+        }
 
-        if (int2intmap_unset(&i2imap, 16) < 0)
-                puts("here");
-
-        vp = int2intmap_get(i2imap, 16);
-        if (vp != NULL)
-                printf("%d\n", *vp);
+        for (i = 1; log2[i] >= 0; i++)
+                assert(int2intmap_unset(&i2imap, i) == 0);
 
         int2intmap_free(&i2imap);
 }
